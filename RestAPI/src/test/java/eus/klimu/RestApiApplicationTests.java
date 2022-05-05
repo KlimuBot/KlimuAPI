9e4919dfa56b6d8157eb1be05fdeb56a032f1273
Aitor Alzola <aitor.alzolas@alumni.mondragon.edu>
2022-05-05 09:21:29 +0200

    Changed directories

---
+++ RestAPI/src/test/java/eus/klimu/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package eus.klimu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
