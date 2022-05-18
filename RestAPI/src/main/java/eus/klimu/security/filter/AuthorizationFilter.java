da23a453e4b36698de0f7bc170896ef00259e3ee
JonNavaridas <jon.navaridas@alumni.mondragon.edu>
2022-05-18 08:21:05 +0200

    Token refresh via response

--- RestAPI/src/main/java/eus/klimu/security/filter/AuthorizationFilter.java
+++ RestAPI/src/main/java/eus/klimu/security/filter/AuthorizationFilter.java
@@ -11,14 +11,10 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Slf4j
 public class AuthorizationFilter extends OncePerRequestFilter {
-
-    private static final String ACCESS_TOKEN_HEADER = "accessToken";
-    private static final String REFRESH_TOKEN_HEADER = "refreshToken";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -26,32 +22,29 @@ public class AuthorizationFilter extends OncePerRequestFilter {
         if (
                 !request.getServletPath().startsWith("/css") &&
                 !request.getServletPath().startsWith("/js") &&
-                !request.getServletPath().startsWith("/media") &&
-                !request.getServletPath().startsWith("/login") &&
-                !request.getServletPath().equals("/user/create")&&
-                !request.getServletPath().equals("/login/sign-up")
+                !request.getServletPath().startsWith("/media")
         ) {
-            HttpSession session = request.getSession();
             TokenManagement tokenManagement = new TokenManagement();
 
             // Check the access token.
-            String accessToken = (String) session.getAttribute(ACCESS_TOKEN_HEADER);
+            String accessToken = request.getHeader(TokenManagement.ACCESS_TOKEN_HEADER);
+
             if (accessToken != null && accessToken.startsWith(TokenManagement.TOKEN_SIGNATURE_NAME)) {
                 try {
                     UsernamePasswordAuthenticationToken authToken = tokenManagement.getUsernamePasswordToken(accessToken);
                     SecurityContextHolder.getContext().setAuthentication(authToken);
                 } catch (Exception accessException) {
-                    String refreshToken = (String) session.getAttribute(REFRESH_TOKEN_HEADER);
+                    String refreshToken = request.getHeader(TokenManagement.REFRESH_TOKEN_HEADER);
                     try {
                         UsernamePasswordAuthenticationToken authToken = tokenManagement.getUsernamePasswordToken(refreshToken);
                         SecurityContextHolder.getContext().setAuthentication(authToken);
 
                         // Generate a new access and refresh token for the user.
                         User user = tokenManagement.getUserFromToken(refreshToken);
-                        tokenManagement.setTokenOnSession(
+                        tokenManagement.setTokenOnResponse(
                                 tokenManagement.generateToken(user, request.getRequestURL().toString(), TokenManagement.ACCESS_TIME),
                                 tokenManagement.generateToken(user, request.getRequestURL().toString(), TokenManagement.REFRESH_TIME),
-                                request.getSession()
+                                response
                         );
                     } catch (Exception refreshException) {
                         response.setHeader("error", refreshException.getMessage());
@@ -60,9 +53,10 @@ public class AuthorizationFilter extends OncePerRequestFilter {
                 }
             }
         }
-        if (request.getParameter(ACCESS_TOKEN_HEADER) != null && request.getParameter(REFRESH_TOKEN_HEADER) != null) {
-            response.setHeader(ACCESS_TOKEN_HEADER, request.getParameter(ACCESS_TOKEN_HEADER));
-            response.setHeader(REFRESH_TOKEN_HEADER, request.getParameter(REFRESH_TOKEN_HEADER));
+        if (request.getParameter(TokenManagement.ACCESS_TOKEN_HEADER) != null &&
+                request.getParameter(TokenManagement.REFRESH_TOKEN_HEADER) != null) {
+            response.setHeader(TokenManagement.ACCESS_TOKEN_HEADER, request.getParameter(TokenManagement.ACCESS_TOKEN_HEADER));
+            response.setHeader(TokenManagement.REFRESH_TOKEN_HEADER, request.getParameter(TokenManagement.REFRESH_TOKEN_HEADER));
         }
         filterChain.doFilter(request, response);
     }
